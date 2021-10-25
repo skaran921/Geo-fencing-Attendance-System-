5b752bf137ee8866bb83d1cc0f6a9391536ac8c0
bishnoia03 <bishnoia03@gmail.com>
2021-10-25 18:52:01 +0530

    new_alert_modal_design

--- index.php
+++ index.php
@@ -11,8 +11,12 @@ session_start();
     <title>Login - Geofencing</title>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="./logo.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://use.fontawesome.com/4c38b3bc58.js"></script>
+    
+    
 </head>
 
 <body>
@@ -51,6 +55,32 @@ session_start();
         </div>
         <!---main form div--->
     </center>
+    <!-- Modal -->
+<div class="modal fade" id="myModal" role="dialog">
+  <div class="modal-dialog">
+  
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-body" style="padding-top:0;">
+      <div class="row" style="background-color:#d9534f;padding:20px;">
+      <center><img src="images/cross.png" width="100px" height="100px" alt=""></center>
+      </div>
+      <div class="message">
+          <center><h2>Oh snap!</h2></center>
+      </div>
+      <div class="message">
+          <center><h4 style="color:#ccc;">Your username or password wrong</h4></center>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <center><button type="button" style="border-radius:40px;width:250px;" class="btn-danger" data-dismiss="modal">OK</button></center>
+      </div>
+    </div>
+    
+  </div>
+</div>
+  
+
 </body>
 <script>
     var overlay = document.getElementById("overlay");
@@ -78,7 +108,7 @@ if (isset($_POST["go"])) {
 
 
         <script>
-            alert("Sorry, Username And Password Not Matched!!!")
+            $('#myModal').modal('show');
         </script>
 
 <?php
